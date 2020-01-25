5756b86b6ddca07fdce60d92a8cdb83188830340
abhi31297 <abhishek31297@gmail.com>
2020-01-25 10:54:44 +0530

    Files added

---
+++ Employee/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class Employee
+    {
+        private string _Id;
+        private string _Name;
+        private string _DepartmentName;
+
+        public delegate void EmpMessageFired(string message);
+
+        public Employee(string Id, string Name, string DepartmentName)
+        {
+            _Id = Id;
+            _Name = Name;
+            _DepartmentName = DepartmentName;
+        }
+        public string GetId()
+        {
+            MessageFired(_Id);
+            return _Id;
+        }
+        public string GetName()
+        {
+            MessageFired2(_Name);
+            return _Name;
+        }
+        public string GetDepartmentName()
+        {
+            MessageFired3(_DepartmentName);
+            return _DepartmentName;
+        }
+        public event EmpMessageFired MessageFired;
+        public event EmpMessageFired MessageFired2;
+        public event EmpMessageFired MessageFired3;
+
+        public void update(string msg)
+        {
+            //was confused how to create an overloaded method for two string variables.
+        }
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Id");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Enter your name");
+            string n = Console.ReadLine();
+
+            Console.WriteLine("Enter you Department Name");
+            string d = Console.ReadLine();
+
+            Employee emp = new Employee(a, n, d);
+            emp.MessageFired += new Employee.EmpMessageFired(OnMessageFired);
+            emp.MessageFired2 += new Employee.EmpMessageFired(OnMessageFired2);
+            emp.MessageFired3 += new Employee.EmpMessageFired(OnMessageFired3);
+
+            Console.WriteLine(emp.GetId());
+            Console.WriteLine(emp.GetName());
+            Console.WriteLine(emp.GetDepartmentName());
+
+            static void OnMessageFired(string message)
+            {
+                Console.WriteLine("GetId() method called");
+            }
+            static void OnMessageFired2(string message)
+            {
+                Console.WriteLine("GetName() method called");
+            }
+            static void OnMessageFired3(string message)
+            {
+                Console.WriteLine("GetDepartmentName() method called");
+            }
+        }
+    }
+}
